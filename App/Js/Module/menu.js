6f40816d8a7f80a65811f7bfe1ce6f5cdea75229
MinhChanh6 <hominhchanh08@gmail.com>
2022-01-21 20:15:23 +0700

    Update

---
+++ App/Js/Module/menu.js
@@ -0,0 +1,54 @@
+export default function handleMenu () {
+    const menuTimeLine = gsap.timeline();
+
+    const menuItems = document.querySelectorAll('.menu-black__item');
+    const menuSocialItems = document.querySelectorAll('.menu-grey__social > a')
+    const burgerMenu = document.querySelector('.burger')
+    const burgerMenuClose = document.querySelector('.menu-black__close')
+    const menu = document.querySelector('.menu')
+    const svgIcon = document.getElementById('circle');
+    const svgArrow = document.getElementById('arrow');
+
+    menuTimeLine.set(svgIcon, {
+        duration: 1,
+        strokeDashoffset: "0",
+        ease: 'expo.inOut',
+    },"=0.75")
+
+    menuTimeLine.fromTo(svgArrow, {
+        opacity: 0
+    }, {opacity: 1}, "-=0.25")
+
+    menuTimeLine.to(menu, {
+        right: '0'
+    }, "-=1.5")
+
+    menuTimeLine.from(menuSocialItems, {
+        duration: 0.5,
+        opacity: 0,
+        y: 10,
+        stagger: 0.1,
+        ease: 'expo.inOut',
+    }, "-=0.75");
+
+
+    menuTimeLine.from(menuItems, {
+        duration: 1,
+        opacity: 0,
+        y: 20,
+        stagger: 0.2,
+        ease: 'expo.inOut',
+    }, "-=1");
+
+
+
+
+    menuTimeLine.reverse(-2);
+
+
+    const handleOpenMenu = () => {
+        menuTimeLine.reversed(!menuTimeLine.reversed());
+    }
+    burgerMenuClose.addEventListener('click', handleOpenMenu);
+    burgerMenu.addEventListener('click', handleOpenMenu);
+}
---
+++ App/Js/index.js
@@ -0,0 +1,6 @@
+import handleMenu from './Module/menu.js';
+import createCanvas from './Module/canvas.js'
+window.addEventListener('DOMContentLoaded', () => {
+    handleMenu();
+    createCanvas();
+});
